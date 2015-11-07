949b788beb17a45e340e7c3c8fe83138416dc80f
Don Johnson <copyleftdev@gmail.com>
2015-11-07 09:37:12 -0800

    completed limiting example

---
+++ src/rate_limiting.go
@@ -0,0 +1,40 @@
+package main
+
+import "time"
+import "fmt"
+
+func main() {
+	requests := make(chan int, 1000)
+	for i := 1; i <= 1000; i++ {
+		requests <- i
+	}
+	close(requests)
+
+	limiter := time.Tick(time.Millisecond * 200)
+
+	for req := range requests {
+		<-limiter
+		fmt.Println("request", req, time.Now())
+	}
+	burstyLimiter := make(chan time.Time, 3)
+
+	for i := 0; i < 3; i++ {
+		burstyLimiter <- time.Now()
+	}
+
+	go func() {
+		for t := range time.Tick(time.Millisecond * 200) {
+			burstyLimiter <- t
+		}
+	}()
+
+	burstyRequests := make(chan int, 1000)
+	for i := 1; i <= 1000; i++ {
+		burstyRequests <- i
+	}
+	close(burstyRequests)
+	for req := range burstyRequests {
+		<-burstyLimiter
+		fmt.Println("request", req, time.Now())
+	}
+}
